3d08ac65671e57de047ebeaffa76fb66df23b95e
cherish <xieqin@qiniu.com>
2019-09-30 16:59:32 +0800

    override counter

--- examples/flutter_layout/lib/main.dart
+++ examples/flutter_layout/lib/main.dart
@@ -77,33 +77,33 @@ class MyButton extends StatelessWidget {
   }
 }
 
-class Counter extends StatefulWidget {
-  @override
-  _CounterState createState () => _CounterState();
-}
+// class Counter extends StatefulWidget {
+//   @override
+//   _CounterState createState () => _CounterState();
+// }
 
-class _CounterState extends State<Counter> {
-  int _counter = 0;
+// class _CounterState extends State<Counter> {
+//   int _counter = 0;
 
-  void _increment () {
-    setState(() {
-      _counter ++;
-    });
-  }
+//   void _increment () {
+//     setState(() {
+//       _counter ++;
+//     });
+//   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      children: <Widget>[
-        RaisedButton (
-          onPressed: _increment,
-          child: Text('Increment'),
-        ),
-        Text('Count: $_counter'),
-      ],
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Row(
+//       children: <Widget>[
+//         RaisedButton (
+//           onPressed: _increment,
+//           child: Text('Increment'),
+//         ),
+//         Text('Count: $_counter'),
+//       ],
+//     );
+//   }
+// }
 
 class CounterDisplay extends StatelessWidget {
   CounterDisplay({this.count});
