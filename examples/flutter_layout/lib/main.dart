5ee7a79d92b1bc2264ce48a58bb44317180d9d1b
cherish <xieqin@qiniu.com>
2019-09-30 17:00:51 +0800

    remove unused code

--- examples/flutter_layout/lib/main.dart
+++ examples/flutter_layout/lib/main.dart
@@ -77,34 +77,6 @@ class MyButton extends StatelessWidget {
   }
 }
 
-// class Counter extends StatefulWidget {
-//   @override
-//   _CounterState createState () => _CounterState();
-// }
-
-// class _CounterState extends State<Counter> {
-//   int _counter = 0;
-
-//   void _increment () {
-//     setState(() {
-//       _counter ++;
-//     });
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Row(
-//       children: <Widget>[
-//         RaisedButton (
-//           onPressed: _increment,
-//           child: Text('Increment'),
-//         ),
-//         Text('Count: $_counter'),
-//       ],
-//     );
-//   }
-// }
-
 class CounterDisplay extends StatelessWidget {
   CounterDisplay({this.count});
 
